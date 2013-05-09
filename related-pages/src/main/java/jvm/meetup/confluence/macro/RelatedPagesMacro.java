fd0da5c544dc3dbaa3197643754777cce8d3c45a
ukuhnhardt <ukuhnhardt@gmail.com>
2013-05-09 14:55:16 +1000

    added parameter

--- related-pages/src/main/java/jvm/meetup/confluence/macro/RelatedPagesMacro.java
+++ related-pages/src/main/java/jvm/meetup/confluence/macro/RelatedPagesMacro.java
@@ -6,11 +6,18 @@ import com.atlassian.confluence.labels.LabelManager;
 import com.atlassian.confluence.labels.Labelable;
 import com.atlassian.confluence.macro.Macro;
 import com.atlassian.confluence.macro.MacroExecutionException;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import com.opensymphony.util.TextUtils;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class RelatedPagesMacro implements Macro {
+
+    public static final String PARAM_LOGIC = "logic";
 
     private final LabelManager labelManager;
 
@@ -22,7 +29,14 @@ public class RelatedPagesMacro implements Macro {
     public String execute(Map<String, String> parameters, String body, ConversionContext conversionContext) throws MacroExecutionException {
 
         List<Label> labels = conversionContext.getPageContext().getEntity().getLabels();
-        List<? extends Labelable> contentWithLabel = labelManager.getContentForAllLabels(labels, 10, 1);
+        Set<Labelable> contentWithLabel = new HashSet<Labelable>();
+        if (TextUtils.stringSet(parameters.get(PARAM_LOGIC)) && parameters.get(PARAM_LOGIC).equalsIgnoreCase("AND")){
+            contentWithLabel.addAll(labelManager.getContentForAllLabels(labels, 1000, 0)) ;
+        } else {
+            for (Label label : labels ){
+                contentWithLabel.addAll(labelManager.getContentForLabel(label, 1000));
+            }
+        }
         return String.format("<h2>Related Pages</h2><p>Found %d pages</p>", contentWithLabel.size());
     }
 
