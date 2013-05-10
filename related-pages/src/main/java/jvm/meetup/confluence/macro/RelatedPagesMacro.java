1811671e8ac8cce25a6695de661fe69f66377fe6
ukuhnhardt <ukuhnhardt@gmail.com>
2013-05-10 15:36:31 +1000

    with template

--- related-pages/src/main/java/jvm/meetup/confluence/macro/RelatedPagesMacro.java
+++ related-pages/src/main/java/jvm/meetup/confluence/macro/RelatedPagesMacro.java
@@ -1,15 +1,23 @@
 package jvm.meetup.confluence.macro;
 
 import com.atlassian.confluence.content.render.xhtml.ConversionContext;
+import com.atlassian.confluence.core.ContentEntityObject;
 import com.atlassian.confluence.labels.Label;
 import com.atlassian.confluence.labels.LabelManager;
 import com.atlassian.confluence.labels.Labelable;
 import com.atlassian.confluence.macro.Macro;
 import com.atlassian.confluence.macro.MacroExecutionException;
+import com.atlassian.confluence.pages.AbstractPage;
+import com.atlassian.confluence.renderer.radeox.macros.MacroUtils;
+import com.atlassian.confluence.util.velocity.VelocityUtils;
+import com.atlassian.velocity.VelocityContextUtils;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.opensymphony.util.TextUtils;
 
+import javax.annotation.Nullable;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -28,7 +36,8 @@ public class RelatedPagesMacro implements Macro {
     @Override
     public String execute(Map<String, String> parameters, String body, ConversionContext conversionContext) throws MacroExecutionException {
 
-        List<Label> labels = conversionContext.getPageContext().getEntity().getLabels();
+        ContentEntityObject self = conversionContext.getPageContext().getEntity();
+        List<Label> labels = self.getLabels();
         Set<Labelable> contentWithLabel = new HashSet<Labelable>();
         if (TextUtils.stringSet(parameters.get(PARAM_LOGIC)) && parameters.get(PARAM_LOGIC).equalsIgnoreCase("AND")){
             contentWithLabel.addAll(labelManager.getContentForAllLabels(labels, 1000, 0)) ;
@@ -37,7 +46,15 @@ public class RelatedPagesMacro implements Macro {
                 contentWithLabel.addAll(labelManager.getContentForLabel(label, 1000));
             }
         }
-        return String.format("<h2>Related Pages</h2><p>Found %d pages</p>", contentWithLabel.size());
+        Map<String, Object> context = MacroUtils.defaultVelocityContext();
+        Set<AbstractPage> pages = new HashSet<AbstractPage>();
+        for (Labelable ceo : contentWithLabel){
+            if (ceo instanceof AbstractPage && !ceo.equals(self)){
+                pages.add((AbstractPage)ceo );
+            }
+        }
+        context.put("pages", pages);
+        return VelocityUtils.getRenderedTemplate("templates/related-pages.vm", context);
     }
 
     @Override
